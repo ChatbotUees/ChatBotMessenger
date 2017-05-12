19e773d0205feb556a616491e9d039238d21dd06
Hector <hec.zenemij@gmail.com>
2017-05-12 17:37:54 -0500

    mejoras

--- app/config/config.js
+++ app/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   access: {    
-    fb_page_token:'EAASoUKbFkdYBAFUPwdkYo8Rr3XZCWyZCQeZAePycIDctZAHvTT9sZB04JO8ZAT47sgpsvQ4QNoiFYJAyQEetn64ZAWlK0QJxTF9uZAsIOoghTVtWmHIOS0NwZAbu5ZBmXiPXJK4CgHg7lmDZBnIWDy54NlqY5eF6iCaPSORXGl3B1OZBZC5KrvfyJEiJU',
+    fb_page_token:'EAASoUKbFkdYBADYM5UWVlJ2oTDWNLjPxXEuunxZAkS9nwhLMaDfPZAZA53yR6PkJUdfmwm5VNGl9i1xPZBRYfVqY0Swt3dczJP4cJtalT5XKAvIzklRTb9RmLGV2vCQUrTGD2yqZAAKeBIlKViYqqwPVVRcDpIRubMb6qWnb8AYwq1v72VhcS',
     fb_verify_token: 'michatbothector081993',
     //fb_app_secret: '025c4bd7149c006f5adb3c6044a2a162',
     fb_app_secret: '025c4bd7149c006f5adb3c6044a2a16234545',
