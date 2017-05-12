684190ab4400ff54f338d6bca3623dd395b56ed5
Hector <hec.zenemij@gmail.com>
2017-05-12 08:45:59 -0500

    mejoras

--- app/config/config.js
+++ app/config/config.js
@@ -9,5 +9,13 @@ module.exports = {
   database: {
     domain: 'admin:admin@ds111441.mlab.com:11441',
     name: 'mydb'
+  },
+
+  access: {    
+    fb_page_token:'EAASoUKbFkdYBAFUPwdkYo8Rr3XZCWyZCQeZAePycIDctZAHvTT9sZB04JO8ZAT47sgpsvQ4QNoiFYJAyQEetn64ZAWlK0QJxTF9uZAsIOoghTVtWmHIOS0NwZAbu5ZBmXiPXJK4CgHg7lmDZBnIWDy54NlqY5eF6iCaPSORXGl3B1OZBZC5KrvfyJEiJU',
+    fb_verify_token: 'michatbothector081993',
+    fb_app_secret: '025c4bd7149c006f5adb3c6044a2a162',
+    api_ai_client_access_token: 'fdc0cc6b3bfd46f9835f5566e06f8464'
   }
+
 }
