e9117295c94008e02aad475b522481e84f24621e
Hector <hec.zenemij@gmail.com>
2017-06-16 14:58:15 -0500

    cambio-token

--- app/config/config.js
+++ app/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   access: {    
     //fb_page_token:'EAASoUKbFkdYBADYM5UWVlJ2oTDWNLjPxXEuunxZAkS9nwhLMaDfPZAZA53yR6PkJUdfmwm5VNGl9i1xPZBRYfVqY0Swt3dczJP4cJtalT5XKAvIzklRTb9RmLGV2vCQUrTGD2yqZAAKeBIlKViYqqwPVVRcDpIRubMb6qWn,b8AYwq1v72VhcS',
-    fb_page_token:'EAASoUKbFkdYBAHBIlsJSeuR80ZAToYPXqJPpKsp58YNxtJt4iLknUhd4jIvMN7ZCX3DzQV0WggZAr3SiiSWxrgQveo58c9pq1iwnW3Y9NqF1mLwFCykwsRjapS7ad18tiEgprijU641lwf2iqtwQZCm7sl8vMEyGTIXEgkDlzrGLKlZAZC4XEE';
+    fb_page_token:'EAASoUKbFkdYBAHBIlsJSeuR80ZAToYPXqJPpKsp58YNxtJt4iLknUhd4jIvMN7ZCX3DzQV0WggZAr3SiiSWxrgQveo58c9pq1iwnW3Y9NqF1mLwFCykwsRjapS7ad18tiEgprijU641lwf2iqtwQZCm7sl8vMEyGTIXEgkDlzrGLKlZAZC4XEE',
     fb_verify_token: 'michatbothector081993',
     //fb_app_secret: '025c4bd7149c006f5adb3c6044a2a162',
     fb_app_secret: '025c4bd7149c006f5adb3c6044a2a16234545',
